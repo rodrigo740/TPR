3bb92d4b8a315ff74ad6048f224247eb50d82013
rodrigo740 <rodrigomartins@ua.pt>
2022-11-04 16:38:33 +0000

    clean

--- irc_server.py
+++
@@ -1,6 +0,0 @@
-import miniirc
-import concurrent.futures
-
-
-
-irc = miniirc.IRC('127.0.0.1', 6667, 'my-bot', ['#python'], ns_identity=('bot', 'bot2'), executor=concurrent.futures.ThreadPoolExecutor())
