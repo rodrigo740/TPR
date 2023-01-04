d819b7b354d47fd2846b3848d2a4fbfc7867b0af
rodrigo740 <rodrigomartins@ua.pt>
2023-01-04 15:57:46 +0000

    fix listenner

--- bots/listener.py
+++ bots/listener.py
@@ -17,21 +17,40 @@ while True:
     text = irc.get_response()
     cmd = text.split(':')
     #print(cmd)
-    if len(cmd) == 3:
-        cmd = cmd[2].strip().replace('$', ' ')
-        print("Got this command: " + cmd)
-        try:
-            if ' ' in cmd:
-                cmd = cmd.split(' ')
-                print("cmd splitted: " + str(cmd))
-                result = subprocess.run(cmd, stdout=subprocess.PIPE)
-            else:
-                result = subprocess.run([cmd, ], stdout=subprocess.PIPE)
-            #print(result.stdout)
-            res = "".join(result.stdout.decode('utf-8').split())
-            #res = ''.join(str(ord(c)) for c in result.stdout.decode('utf-8'))
-            irc.send(channel, res)
-        except:
-            print("Something wrong with the cmd, try again!")
+    if len(cmd) == 3 or len(cmd) == 4:
+        if len(cmd) == 4:
+            dest = ":" + cmd[3]
+            cmd = cmd[2].strip().replace('$', ' ')
+            cmd += dest.strip()
+            print("Got this command: " + cmd)
+            try:
+                if ' ' in cmd:
+                    cmd = cmd.split(' ')
+                    print("cmd splitted: " + str(cmd))
+                    result = subprocess.run(cmd, stdout=subprocess.PIPE)
+                else:
+                    result = subprocess.run([cmd, ], stdout=subprocess.PIPE)
+                #print(result.stdout)
+                res = "".join(result.stdout.decode('utf-8').split())
+                #res = ''.join(str(ord(c)) for c in result.stdout.decode('utf-8'))
+                irc.send(channel, res)
+            except:
+                print("Something wrong with the cmd, try again!")
+        else:
+            cmd = cmd[2].strip().replace('$', ' ')
+            print("Got this command: " + cmd)
+            try:
+                if ' ' in cmd:
+                    cmd = cmd.split(' ')
+                    print("cmd splitted: " + str(cmd))
+                    result = subprocess.run(cmd, stdout=subprocess.PIPE)
+                else:
+                    result = subprocess.run([cmd, ], stdout=subprocess.PIPE)
+                #print(result.stdout)
+                res = "".join(result.stdout.decode('utf-8').split())
+                #res = ''.join(str(ord(c)) for c in result.stdout.decode('utf-8'))
+                irc.send(channel, res)
+            except:
+                print("Something wrong with the cmd, try again!")
  
     #if "PRIVMSG" in text and channel in text and "hello" in text:
